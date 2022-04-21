d8f72d15439bf1043b3d31c3a43a73bbfc1a02c6
martin023 <mainamartin023@gmail.com>
2022-04-21 12:19:14 +0300

    This commit will add upvoting logic

--- src/app/upvote/upvote.component.ts
+++ src/app/upvote/upvote.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-upvote',
@@ -7,6 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UpvoteComponent implements OnInit {
 
+
+  @Input() testName: string;
+  NumberOfUpvotes: number = 0;
+
+  UpvoteButtonClick() {
+    this.NumberOfUpvotes++;
+  }
   constructor() { }
 
   ngOnInit(): void {
